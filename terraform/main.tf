77709093ccfa3d2bc8546c467a5bd5fb938897a2
asdqwce <lee895787@gmail.com>
2026-06-09 12:44:59 +0900

    feat: 마스터 노드 user_data ECR Secret 자동갱신 crontab 추가

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "default" {
+/data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
@@ -27,10 +27,6 @@ data "aws_subnets" "default" {
 resource "aws_key_pair" "k8s_key" {
   key_name   = "${local.name_prefix}-k8s-key"
   public_key = file(var.public_key_path)
-
-  lifecycle {
-    ignore_changes = [public_key]
-  }
 
   tags = merge(local.common_tags, {
     Name = "${local.name_prefix}-k8s-key"
@@ -94,15 +90,20 @@ resource "aws_security_group" "k8s_sg" {
 resource "aws_instance" "master" {
   ami                    = var.ami_id
   instance_type          = var.master_instance_type
-  iam_instance_profile   = aws_iam_instance_profile.k8s_node.name
   key_name               = aws_key_pair.k8s_key.key_name
   vpc_security_group_ids = [aws_security_group.k8s_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
   root_block_device {
     volume_size = var.master_volume_size
     volume_type = var.volume_type
   }
 
+  user_data = <<-USERDATA
+    #!/bin/bash
+    (crontab -l 2>/dev/null; echo "0 */10 * * * /bin/bash -c 'ECR_PASSWORD=\$(aws ecr get-login-password --region ap-northeast-2) && for ns in ticketing-auth ticketing-concert ticketing-reservation ticketing-payment ticketing-ticket ticketing-notification ticketing-dashboard; do kubectl delete secret ecr-registry -n \${ns} --ignore-not-found; kubectl create secret docker-registry ecr-registry --docker-server=941141115079.dkr.ecr.ap-northeast-2.amazonaws.com --docker-username=AWS --docker-password=\${ECR_PASSWORD} -n \${ns}; done'") | crontab -
+  USERDATA
+  
   tags = merge(local.common_tags, {
     Name = "${local.name_prefix}-master"
     Role = "master"
@@ -114,9 +115,9 @@ resource "aws_instance" "worker" {
   count                  = var.worker_count
   ami                    = var.ami_id
   instance_type          = var.worker_instance_type
-  iam_instance_profile   = aws_iam_instance_profile.k8s_node.name
   key_name               = aws_key_pair.k8s_key.key_name
   vpc_security_group_ids = [aws_security_group.k8s_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   depends_on             = [aws_instance.master]
 
   root_block_device {
@@ -127,6 +128,21 @@ resource "aws_instance" "worker" {
   tags = merge(local.common_tags, {
     Name = "${local.name_prefix}-worker-${count.index + 1}"
     Role = "worker"
+  })
+}
+
+# 서비스 이미지 저장소
+resource "aws_ecr_repository" "service" {
+  for_each = var.ecr_repositories
+
+  name         = "${each.key}"
+  force_delete = var.ecr_force_delete
+
+  image_tag_mutability = "MUTABLE"
+
+  tags = merge(local.common_tags, {
+    Name    = "${local.name_prefix}-${each.key}"
+    Service = each.key
   })
 }
 
@@ -167,10 +183,6 @@ resource "aws_lb_listener" "kong_http" {
   port              = var.nlb_listener_port
   protocol          = "TCP"
 
-  lifecycle {
-    ignore_changes = [default_action]
-  }
-
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.kong.arn
@@ -210,12 +222,10 @@ output "security_group_id" {
   value = aws_security_group.k8s_sg.id
 }
 
-output "k8s_node_role_name" {
-  value = aws_iam_role.k8s_node.name
-}
-
-output "k8s_node_instance_profile_name" {
-  value = aws_iam_instance_profile.k8s_node.name
+output "ecr_repository_urls" {
+  value = {
+    for name, repo in aws_ecr_repository.service : name => repo.repository_url
+  }
 }
 
 output "nlb_dns_name" {
@@ -228,4 +238,67 @@ output "nlb_target_group_arn" {
 
 output "workspace" {
   value = terraform.workspace
+}
+
+# EC2가 ECR에 접근하기 위한 IAM Role
+resource "aws_iam_role" "ec2_role" {
+  name = "${local.name_prefix}-ec2-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-ec2-role"
+  })
+}
+
+# ECR 읽기 권한 부여
+resource "aws_iam_role_policy_attachment" "ecr_read" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# IAM Instance Profile (EC2에 Role 연결)
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "${local.name_prefix}-ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+# 마스터 고정 IP
+resource "aws_eip" "master" {
+  instance = aws_instance.master.id
+  domain   = "vpc"
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-master-eip"
+  })
+}
+
+output "master_eip" {
+  value = aws_eip.master.public_ip
+}
+
+resource "aws_iam_role_policy" "ebs_csi" {
+  name = "${local.name_prefix}-ebs-csi-policy"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "ec2:CreateVolume",
+        "ec2:DeleteVolume",
+        "ec2:AttachVolume",
+        "ec2:DetachVolume",
+        "ec2:DescribeVolumes",
+        "ec2:DescribeInstances",
+        "ec2:CreateTags"
+      ]
+      Resource = "*"
+    }]
+  })
 }
